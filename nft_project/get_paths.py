b860ece0d362dfcad8aa58b7898bdf007567f02f
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2022-02-04 21:30:48 -0800

    added some folder stuff

--- nft_project/get_paths.py
+++ nft_project/get_paths.py
@@ -16,3 +16,21 @@ def get_attributes_from_nft_folder():
 
 def construct_path(attribute, image_name):
     return f'{NFT_FOLDER}/{attribute}/{image_name}'
+
+def ensure_output_folders_exist():
+    '''if these folders do not exsist create them'''
+    if not os.path.exists('nft_project/results'):
+        os.mkdir('nft_project/results')
+        os.mkdir('nft_project/results/json')
+        os.mkdir('nft_project/results/images')
+        os.mkdir('nft_project/results/csv')
+
+    if not os.path.exists('nft_project/results/json'):
+        os.mkdir('nft_project/results/json')
+
+    if not os.path.exists('nft_project/results/images'):
+        os.mkdir('nft_project/results/images')
+
+    if not os.path.exists('nft_project/results/csv'):
+        os.mkdir('nft_project/results/csv')
+        
